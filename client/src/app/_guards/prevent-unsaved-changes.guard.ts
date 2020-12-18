d9753df8f546e28bc46b522399a94822f77be901
adeelsnet <adeelsnet@users.noreply.github.com>
2020-12-18 01:00:29 -0500

    Added Edit functionality and Updated API calls

---
+++ client/src/app/_guards/prevent-unsaved-changes.guard.ts
@@ -0,0 +1,25 @@
+import { Injectable } from '@angular/core';
+import { CanDeactivate, ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree } from '@angular/router';
+import { Observable } from 'rxjs';
+import { MemberEditComponent } from '../members/member-edit/member-edit.component';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class PreventUnsavedChangesGuard implements CanDeactivate<unknown> {
+  // canDeactivate(
+  //   component: unknown,
+  //   currentRoute: ActivatedRouteSnapshot,
+  //   currentState: RouterStateSnapshot,
+  //   nextState?: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
+  //   return true;
+  // }
+  canDeactivate(component: MemberEditComponent): boolean {
+
+    if (component.editForm.dirty) {
+      return confirm('Are you sure you want to continue, any unsaved changes will be lost');
+    }
+    return true;
+  }
+
+}
--- client/src/app/_services/members.service.ts
+++ client/src/app/_services/members.service.ts
@@ -1,6 +1,7 @@
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
+import { Observable, of } from 'rxjs';
+import { map } from 'rxjs/operators';
 import { environment } from 'src/environments/environment';
 import { Member } from '../_models/member';
 
@@ -17,17 +18,43 @@ const httpOptions = {
 export class MembersService {
   baseUrl = environment.apiUrl;
 
+  /* for accessing members everytime we look at a member an API call triggers, to avoid that and manage state
+  of the application we implment member array and below methods with some checks*/
+  members: Member[] = [];
+
   constructor(private http: HttpClient) { }
 
   getMembers() {
     // return this.http.get<Member[]>(this.baseUrl + 'users', httpOptions); Because we implement interceptor and provide header there
-
-    return this.http.get<Member[]>(this.baseUrl + 'users');
+    // return this.http.get<Member[]>(this.baseUrl + 'users');
+    if (this.members.length > 0) {
+      return of(this.members); // of() returns observable
+    }
+    return this.http.get<Member[]>(this.baseUrl + 'users').pipe(
+      map(members => {
+        this.members = members;
+        return members;
+      })
+    );
   }
 
   getMember(userName: string) {
-  // return this.http.get<Member>(this.baseUrl + 'users/' + userName , httpOptions);  Because we implement interceptor and provide header there
+    // return this.http.get<Member>(this.baseUrl + 'users/' + userName , httpOptions);  Because we implement interceptor and provide header there
 
+    const member = this.members.find(x => x.username === userName);
+    if (member !== undefined) {
+      return of(member);
+    }
     return this.http.get<Member>(this.baseUrl + 'users/' + userName);
+  }
+
+  updateMember(member: Member) {
+    // return this.http.put(this.baseUrl + 'users', member);
+    return this.http.put(this.baseUrl + 'users', member).pipe(
+      map(() =>{
+        const index = this.members.indexOf(member);
+        this.members[index] = member;
+      })
+    );
   }
 }
